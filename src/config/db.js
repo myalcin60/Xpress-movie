a1b62aeb119d9acdfabe3ab4edcc5f1c33c401d5
yalcin <yalcinmusa60@gamil.com>
2025-10-01 20:02:34 +0200

    env updated

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+import mysql from 'mysql2';
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Veritabanı bağlantı hatası:', err);
+  } else {
+    console.log('MySQL veritabanına başarıyla bağlandı!');
+  }
+});
