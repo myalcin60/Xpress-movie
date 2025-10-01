01ccbfd5f9da52182f8faa9474a47d4e29687454
yalcin <yalcinmusa60@gamil.com>
2025-10-01 21:08:11 +0200

    login test

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,34 +23,38 @@ const signup = async (req, res, next) => {
         res.render('signup')
     }
 }
+export const login = (req, res) => {
+  res.json({ message: 'Login endpoint çalışıyor!' });
+};
 
-const login = async (req, res, next) => {
-    const user = await userRepository.getUser(req.body.email)
-    try {
-        if (await bcrypt.compare(req.body.password, user[0].password)) {
 
-            req.session.user = {
-                role: user[0].role,
-                nom: user[0].nom,
-                prenom: user[0].prenom,
-                email: user[0].email,
-                password: user[0].password,
-                id: user[0].id
+// const login = async (req, res, next) => {
+//     const user = await userRepository.getUser(req.body.email)
+//     try {
+//         if (await bcrypt.compare(req.body.password, user[0].password)) {
 
-            };
-            res.redirect('admin')
-        }
-        else {
-            res.render('login',
-                console.log("Password n'est pas correct")
-            )
-        }
-    } catch (error) {
-        console.log(error);
-        res.render('login')
-    }
+//             req.session.user = {
+//                 role: user[0].role,
+//                 nom: user[0].nom,
+//                 prenom: user[0].prenom,
+//                 email: user[0].email,
+//                 password: user[0].password,
+//                 id: user[0].id
 
-}
+//             };
+//             res.redirect('admin')
+//         }
+//         else {
+//             res.render('login',
+//                 console.log("Password n'est pas correct")
+//             )
+//         }
+//     } catch (error) {
+//         console.log(error);
+//         res.render('login')
+//     }
+
+// }
 
 const logout = (req, res, next) => {
     req.session.destroy((err) => {
