eec8eca3915c95d4d667a13f40f099845fd06a68
M Yalcin <yalcin@gmail.com>
2025-10-02 13:50:25 +0200

    home css updated

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -6,17 +6,17 @@ export default function Home() {
 
     return (
         <>
-            <div className="wrapper">
-                <header>
-
-                </header>
-                <main className="content">
-                    <h2> Bienvenu notre platform de films</h2>
-                    <FilmCard />
+            <div className="container-fluid min-vh-100 d-flex flex-column">
+                <main className="content flex-grow-1 d-flex flex-column justify-content-center align-items-center text-center py-4">
+                    <h2 className="mb-4">Bienvenu notre platform de films</h2>
+                    <div className="row w-100 justify-content-center">
+                        <div className="col-12 col-sm-8 col-md-6 col-lg-4">
+                            <FilmCard />
+                        </div>
+                    </div>
                 </main>
-
             </div>
-
         </>
     )
+
 }
