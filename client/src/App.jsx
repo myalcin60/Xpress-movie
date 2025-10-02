0eacc2120edd019148bd2f63cfc0504f89e2514a
M Yalcin <yalcin@gmail.com>
2025-10-02 10:27:58 +0200

    app.jsx uodated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,23 +5,23 @@ import Footer from './components/Footer/Footer'
 import AppRoutes from './router/AppRoutes'
 import { useEffect, useState } from "react";
 
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 
 
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+// const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 
 
 function App() {
 
 
-  const [instruments, setInstruments] = useState([]);
-  useEffect(() => {
-    getInstruments();
-  }, []);
-  async function getInstruments() {
-    const { data } = await supabase.from("instruments").select();
-    setInstruments(data);
-  }
+  // const [instruments, setInstruments] = useState([]);
+  // useEffect(() => {
+  //   getInstruments();
+  // }, []);
+  // async function getInstruments() {
+  //   const { data } = await supabase.from("instruments").select();
+  //   setInstruments(data);
+  // }
   return (
     <>
       <Header />
