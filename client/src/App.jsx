d898f324a4459706ea87b6d23d53ac72af2499ab
M Yalcin <yalcin@gmail.com>
2025-10-02 10:54:20 +0200

    db mysql/promise added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,23 +5,7 @@ import Footer from './components/Footer/Footer'
 import AppRoutes from './router/AppRoutes'
 import { useEffect, useState } from "react";
 
-// import { createClient } from "@supabase/supabase-js";
-
-
-// const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
-
-
 function App() {
-
-
-  // const [instruments, setInstruments] = useState([]);
-  // useEffect(() => {
-  //   getInstruments();
-  // }, []);
-  // async function getInstruments() {
-  //   const { data } = await supabase.from("instruments").select();
-  //   setInstruments(data);
-  // }
   return (
     <>
       <Header />
