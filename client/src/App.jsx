3491ba0b6e6ff118e90f5b1b84062b5ed0b87f9b
M Yalcin <yalcin@gmail.com>
2025-10-02 13:15:57 +0200

    app botsrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import AppRoutes from './router/AppRoutes'
@@ -16,4 +16,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
