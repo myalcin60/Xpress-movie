20f0e77becc18f70128c801082e10478115eb5e9
M Yalcin <yalcin@gmail.com>
2025-10-02 13:32:28 +0200

    MAIN;JSX BOOTSRAP ADDED

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import AppRoutes from './router/AppRoutes'
